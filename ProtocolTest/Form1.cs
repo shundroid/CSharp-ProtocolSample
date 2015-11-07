38c65a6c3f046219408cf66097c60436e0815a0b
shundroid <ra24483@qb3.so-net.ne.jp>
2015-11-07 14:01:15 +0900

    First commit

---
+++ ProtocolTest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace ProtocolTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey regkey = Registry.ClassesRoot.CreateSubKey(@"protocoltest");
+            regkey.SetValue("", "URL:C# Test Protocol");
+            regkey.SetValue("URL Protocol", "");
+            RegistryKey openCommand = regkey.CreateSubKey("shell").CreateSubKey("open").CreateSubKey("command");
+            openCommand.SetValue("", "\"C:\\Windows\\notepad.exe\" \"%1");
+            openCommand.Close();
+            regkey.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("protocoltest:");
+        }
+    }
+}
